f19323d107549d1dd9b077ba9a99dad22746e291
Felipe Kesley <fkesley.dev@gmail.com>
2024-07-23 13:02:58 -0300

    feat: menu mobile and greensock

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  new Swiper(".swiper-highlights", {
+  const btnBurger = document.querySelector(".btn-burger");
+  const btnClose = document.querySelector(".btn-close");
+  const nav = document.querySelector(".nav");
+  const menu = document.querySelector(".menu");
+  const menuItems = menu.querySelectorAll("li");
+
+  let tl = gsap.timeline({ paused: true, delay: -1 });
+
+  tl.to(nav, {
+    duration: 0.2,
+    autoAlpha: 1,
+    display: "flex",
+    ease: "power2.inOut",
+    onStart: () => (nav.style.display = "flex"),
+    onReverseComplete: () => (nav.style.display = "none"),
+  }).from(
+    menuItems,
+    {
+      duration: 0.5,
+      opacity: 0,
+      y: 100,
+      stagger: 0.1,
+      ease: "power2.inOut",
+    },
+    "-=0.3"
+  );
+
+  btnBurger.addEventListener("click", function () {
+    tl.play(0);
+    btnClose.style.display = "flex";
+  });
+
+  btnClose.addEventListener("click", function () {
+    this.style.display = "none";
+    tl.reverse();
+  });
+  
+    new Swiper(".swiper-highlights", {
     loop: true,
     slidesPerView: 1,
     breakpoints: {
